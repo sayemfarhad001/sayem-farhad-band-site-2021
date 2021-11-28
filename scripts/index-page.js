2f3140d4037a8f308ffa6bad47eaae6f2b2642f8
Sayem Mohd Farhad <sayemfarhad001@gmail.com>
2021-11-27 22:31:10 -0500

    add Comment section and build site structure

---
+++ scripts/index-page.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
