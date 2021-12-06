eb74a403dcc51108d314253756eca8412b1bff73
Sayem Mohd Farhad <sayemfarhad001@gmail.com>
2021-12-05 22:28:20 -0500

    final version

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,119 +1,117 @@
-// Create "Arrays of Show Data"
+// STEP 1 - Create "Arrays of Show Data"
 const dateArray = ['Mon Sept 06 2021', 'Tue Sept 21 2021', 'Fri Oct 15 2021', 'Sat Nov 06 2021', 'Fri Nov 26 2021', 'Wed Dec 15 2021'];
 const venueArray = ['Ronald Lane', 'Pier 3 East', 'View Lounge', 'Hyatt Agency', 'Moscow Center', 'Press Club'];
 const locationArray = ['San Francisco, CA', 'San Francisco, CA', 'San Francisco, CA', 'San Francisco, CA', 'San Francisco, CA', 'San Francisco, CA'];
 const ticketArray = ['BUY TICKETS', 'BUY TICKETS', 'BUY TICKETS', 'BUY TICKETS', 'BUY TICKETS', 'BUY TICKETS'];
 
-// Create an Unordered list within a div with class(shows__container)
+// STEP 2 - Create an Unordered list within a div with class(shows__container)
 let div = document.querySelector(".shows__container");
 let ul = document.createElement("ul");
 ul.classList.add("shows__list");
 div.appendChild(ul);
 
-// Higher order function for adding data from "Arrays of Show Data" above to HTML
-// Total Funtions created - 4
-// Funtion 1/4 - creates "dates" and adds classes
+// STEP 3 - Create Higher order function for adding data from "Arrays of Show Data" above to HTML - Total Funtions created - 4
+
+// Funtion 1/4 - creates "dates" and adds classes [first function elaborately explained]
 dateArray.forEach((element) => {
+    //  A - Create p element, add classes and innerText
     let defaultTitle = document.createElement('p');
     defaultTitle.classList.add('hide__later');
     defaultTitle.classList.add('show__title__date');
 	defaultTitle.innerText = "DATE";
+    //  B - Create p element, add classes and innerText(FROM above DATA ARRAY)
 	let p = document.createElement('p');
 	p.innerText = element;
     p.classList.add('show__date');
+    //  C - Create div element, add class
     let inShow = document.createElement('div');
 	inShow.classList.add('date__div');
+    //  D - Append the created p elements(A and B) to created div element(C)
     inShow.appendChild(defaultTitle);
     inShow.appendChild(p);
-
+    //  E - Create li element, add class
     let show = document.createElement('li');
 	show.classList.add('show');
+    //  F - Append the created div element(C)[includes 'A and B' as Children] to li element(E)
     show.appendChild(inShow);  
-
+    //  G - Append the created li element(E)[includes child(C) and grandchildren(A and B)] to the parent element on HTML
     let showsList = document.querySelector('.shows__list');
     showsList.appendChild(show);
 })
 
 // Funtion 2/4 - creates "venues" and adds classes
 venueArray.forEach((element) => {
-	let showsList = document.querySelector('.shows__list');
-
+    //  A - Create p elements, add classes and innerText
     let defaultTitle = document.createElement('p');
     defaultTitle.classList.add('hide__later');
     defaultTitle.classList.add('show__title__venue');
 	defaultTitle.innerText = "VENUE";
-
 	let p = document.createElement('p');
 	p.innerText = element;
     p.classList.add('show__venue');
-
+    //  B - Create div element, add class
     let inShow = document.createElement('div');
 	inShow.classList.add('venue__div');
-
+    //  C - Append A to B
     inShow.appendChild(defaultTitle);
     inShow.appendChild(p);
-
+    //  D - Select li with class-show AND Append C to it
     let show = document.querySelector('.show');
-  
     show.appendChild(inShow);
-  
+    //  E - Select parent element[ul.shows__list] AND Append D to it
+	let showsList = document.querySelector('.shows__list');  
     showsList.appendChild(show);
 })
 
 // Funtion 3/4 - creates "locations" and adds classes
 locationArray.forEach((element) => {
-	let showsList = document.querySelector('.shows__list');
-
+    //  A - Create p elements, add classes and innerText
     let defaultTitle = document.createElement('p');
     defaultTitle.classList.add('hide__later');
     defaultTitle.classList.add('show__title__location');
 	defaultTitle.innerText = "LOCATION";
-
 	let p = document.createElement('p');
 	p.innerText = element;
     p.classList.add('show__location');
-
+    //  B - Create div element, add class
     let inShow = document.createElement('div');
 	inShow.classList.add('location__div');
-
+    //  C - Append A to B
     inShow.appendChild(defaultTitle);
     inShow.appendChild(p);
-
+    //  D - Select li with class-show AND Append C to it
     let show = document.querySelector('.show');
-  
     show.appendChild(inShow);
-  
+    //  E - Select parent element[ul.shows__list] AND Append D to it
+	let showsList = document.querySelector('.shows__list');  
     showsList.appendChild(show);
 })
 
 // Funtion 4/4 - creates "tickets" and adds classes
 ticketArray.forEach((element) => {
-	let showsList = document.querySelector('.shows__list');
-
+    //  A - Create p elements, add classes and innerText	
     let defaultTitle = document.createElement('p');
     defaultTitle.classList.add('hide__later');
     defaultTitle.classList.add('show__title__ticket');
 	defaultTitle.innerText = "";
-
 	let p = document.createElement('button');
 	p.innerText = element;
     p.classList.add('show__ticket');
-
+    //  B - Create div element, add class
     let inShow = document.createElement('div');
 	inShow.classList.add('ticket__div');
-
+    //  C - Append A to B
     inShow.appendChild(defaultTitle);
     inShow.appendChild(p);
-
+    //  D - Select li with class-show AND Append C to it
     let show = document.querySelector('.show');
-  
     show.appendChild(inShow);
-  
+    //  E - Select parent element[ul.shows__list] AND Append D to it
+    let showsList = document.querySelector('.shows__list');
     showsList.appendChild(show);
 });
 
-
-// Final touch - adding special classes - for styling
+// STEP 4 - Adding special classes - for styling
 let selectFirstDate = document.querySelector('.show__title__date');
 selectFirstDate.classList.add('keep');
 let selectFirstVenue = document.querySelector('.show__title__venue');
@@ -122,4 +120,3 @@ let selectFirstLocation = document.querySelector('.show__title__location');
 selectFirstLocation.classList.add('keep');
 let selectFirstTicket = document.querySelector('.ticket__div');
 selectFirstTicket.classList.add('keep__down');
-
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,4 @@
-// Final JS - Index Page
-
-// Step 1 - create JS Array with 3 default objects
+// 	Step 1 - create an editable Array with 3 default objects
 let commentArray = [{
 	name: "Connor Walton",
 	timeStamp: "02/17/2021",
@@ -17,258 +15,99 @@ let commentArray = [{
 	commentText: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
 }];
 
-// Step 2 - Create function to display default comments
+// 	Step 2 - Create function to display Data from the Array above
 let displayComment = function(){
 	for (i=0; i<commentArray.length; i++) {
-		// Make a new element, and set input default comment		
+		// A - Create p element, add name from above Array and add class		
 		const nameTag = document.createElement('p');
 		nameTag.innerText = commentArray[i].name;
 		nameTag.classList.add("section__form-name");
-
+		// B - Create p element, add timeStamp from above Array and add class
 		const timeStampTag = document.createElement('p');
 		timeStampTag.innerText = commentArray[i].timeStamp;
 		timeStampTag.classList.add("section__form-timeStamp");
-
-		// Create Inner Div	
+		// C - Create INNER div element, add class	
 		const innerDiv = document.createElement('div');
 		innerDiv.classList.add("section__form-innerDiv");
-		
+		// D - Append [A and B] to created INNER div element(C)
 		innerDiv.appendChild(nameTag);
 		innerDiv.appendChild(timeStampTag);
-
+		// E - Create p element, add commentText from above Array and add class
 		const commentTextTag = document.createElement('p');
 		commentTextTag.innerText = commentArray[i].commentText;
 		commentTextTag.classList.add("section__form-commentText");
-		
-		// Create Outer Div
+		// F - Create OUTER div element, add class
 		const outerDiv = document.createElement('div');
 		outerDiv.classList.add("section__form-outerDiv");
-			
+		// G - Append [D and E] to created OUTER div element(F)
 		outerDiv.appendChild(innerDiv);
 		outerDiv.appendChild(commentTextTag);
-
+		// H - Create p element[for logo], add class and NOTHING ELSE FOR NOW
 		const logoTag = document.createElement('p');
 		logoTag.classList.add("section__form-logo");
-
-		// Create Main Div
+		// I - Create MAIN div element, add class
 		const mainDiv = document.createElement('div');
 		mainDiv.classList.add("section__form-mainDiv");
-			
+		// J - Append [G and H] to created MAIN div element(I)			
 		mainDiv.appendChild(logoTag);
 		mainDiv.appendChild(outerDiv);
-
-		// Grab comment section
+		// K - Select parent element [div.shows__list] AND Append J to it
 		const commentSection = document.querySelector(".section__comment");
-		// Add a Complete Comment 
 		commentSection.appendChild(mainDiv);
 	}
 }
 
-// Step 3 - Call the created funtion - before any event
+// 	Step 3 - Call the created funtion - before any event
 displayComment()
 
-
-// Step 4 - Create an event for Clicking COMMENT Button
-
+// 	Step 4 - Create an event for Clicking COMMENT Button
+// 	A - Select BUTTON element AND FORM element
 const submitButton = document.querySelector("#myButton");
 const sectionMain = document.querySelector(".section__form");
-
+//	B
 submitButton.addEventListener('click', (event) => {
-    event.preventDefault()
-
-	// grab text inside input element via query selector
+    // 	I. 	Prevent refreshing the page
+	event.preventDefault()
+	// 	II. Collect value from input element
     const nameValue = document.querySelector("#name").value;
     const commentValue = document.querySelector("#comment").value;
-
-    // validation: ensure comment is lengthy enough
+    // 	III. Validate: ensure length is present
     if (nameValue.length < 1 || commentValue.length < 1) {
-        // add visual feedback
+        //DOM Manipulation to add Visual error border
         const nameTag = document.querySelector("#name");
         nameTag.classList.add('error');
 		nameTag.removeAttribute("placeholder");
 		const commentTag = document.querySelector("#comment");
         commentTag.classList.add('error');
 		commentTag.removeAttribute("placeholder");
-
-        // add message telling user there's an error
-        // sectionMain.appendChild(errorStyle);
         return;
     }
-
+	// 	IV. Clear input fields
 	let clearAll = document.querySelector(".section__comment");
 	clearAll.innerHTML = "";
-
-	// Make new Object format
-	// const commentObject = documrnt.querySelector(".section__form-mainDiv");
-	// commentObject.innerHTML =
-	
-
-
-	// Function for live date
-	// 	GetTime();
-
-	// function GetTime(){
-	//   var CurrentTime = new Date()
-	//   var hour = CurrentTime.getHours()
-	//   var minute = CurrentTime.getMinutes()
-	//   var second = CurrentTime.getSeconds()
-
-	//   if(minute < 10){
-	//     minute = "0" + minute
-	//   }
-
-	//   if(second < 10){
-	//     second = "0" + second
-	//   }
-
-	//   var GetCurrentTime = hour + ":" + minute + ":" + second + " ";
-
-	//   if(hour > 11){
-	//     GetCurrentTime += "p.m."
-	//   }else{
-	//     GetCurrentTime += "a.m."
-	//   }
-
-	//   document.getElementById("CurrentTime").innerHTML = GetCurrentTime;
-	//   setTimeout(GetTime,1000)
-	// }
-
-	// Funtion for timestamp
+	// 	V. Add Funtion for timestamp
 	function getTimeStamp() {
 		var now = new Date();
 		return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear());
  	}
-
-	//  function getTimeStamp() {
-	// 	var now = new Date();
-	// 	return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':'
-	// 				  + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now
-	// 				  .getSeconds()) : (now.getSeconds())));
- 	// }
-
-	// function setTime() {
-	// 	document.getElementById('field').value = getTimeStamp();
-	// }
-
-
-
+	//  VI. Create a new OBJECT to collect INPUT VALUE from FORM INPUT fields and a Time Funtion
 	let newObject = { name: "", timeStamp: "", commentText: "" }
 	newObject.name = nameValue;
 	newObject.timeStamp = getTimeStamp();
 	newObject.commentText = commentValue; 
-
+	//  VII. Add the created OBJECT[VI] to initial ARRAY[STEP 1]
 	commentArray.unshift(newObject);
+	// 	VIII. Call the created funtion[line 60] again - DUE TO CLICK EVENT
 	displayComment()
-
-
-    // make a new element, and set input to to innerText -> this becomes our comment
-    // const nameElement = document.createElement('p');
-    // nameElement.innerText = nameValue;
-   
-	
-	
-	// grab comment section
-    // const commentSection = document.querySelector(".section__comment");
-    
-	// commentSection.prepend(nameElement)
-
-    // upon successful submission, remove error message
-    nameTag.setAttribute("placeHolder", "Enter your name");
+    // 	IX. On Successful Submission, Remove Error Visual and Clear Input Fields
+	const nameTag = document.querySelector("#name");
+	const commentTag = document.querySelector("#comment");
+    // 	a. Remove Error Visual via removal and addition of classes
+	nameTag.setAttribute("placeHolder", "Enter your name");
 	nameTag.removeAttribute("class");
-	
-    commentTag.setAttribute("placeholder", "Add a new comment");
+	commentTag.setAttribute("placeholder", "Add a new comment");
 	commentTag.removeAttribute("class");
+	// 	b. Clear Input Fields
+	nameTag.value = "";
+	commentTag.value = "";
 })
-
-
-
-
-
-
-
-window.onload = () => {
-	const commentForm = document.getElementById("form");
-
-	commentForm.addEventListener("submit", (event) => {
-		event.preventDefault();		//prevent refreshing page
-		
-		const formTarget = event.target;
-
-		const userName = formTarget.name.value;
-		const userComment = formTarget.comment.value;
-
-		console.log(userName, userComment);
-	})
-}
-
-
-// document.getElementById("myButton").addEventListener("click", myFunction);
-
-// function myFunction() {
-//   let addP = document.createElement()
-  
-//   document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
-
-//   var input = document.getElementById("stuffed");
-//   var x = input.value;
-//   document.getElementById("paragraph").innerHTML += "<br>&lt;me&gt; " + x;
-
-//   input.value = '';
-//   input.focus();
-
-// }
-
-
-
-// document.getElementById("myBtn").addEventListener("click", function() {
-//     document.getElementById("demo").innerHTML = "Hello World";
-//   });
-
-
-
-// Empty Comments after Post
-// JS
-// function post() {
-//   var x = document.getElementById("stuffed").value;
-//   document.getElementById("paragraph").innerHTML += "<br>&lt;me&gt; " + x;
-  
-//   document.getElementById("stuffed").value = ''
-// }
-
-// Add to HTML
-/*   
-<input type="text" aria-label="Comments" placeholder="Comments" value="" 
-    style="padding:10px 50px 10px 5px" id="stuffed"></input>
-
-<button onclick="post()" style="text-decoration: none; color: #ffffff; 
-    background-color: #5dbbd4; padding: 12px 10px 12px 10px; border-width: 0px;">Post</button>
-
-
-<p style="color: #000;" id="paragraph"></p> */
-
-
-
-
-
-// AddEVENTLISTENER
-// document.getElementById('postbtn').addEventListener('click', post);
-
-// function post() {
-//   var input = document.getElementById("stuffed");
-//   var x = input.value;
-//   document.getElementById("paragraph").innerHTML += "<br>&lt;me&gt; " + x;
-
-//   input.value = '';
-//   input.focus();
-// }
-
-// HTML INPUT BUTTON
-/* <input type="text" aria-label="Comments" placeholder="Comments" value="" 
-	style="padding:10px 50px 10px 5px" id="stuffed">
-	</input>
-<button id="postbtn" style="text-decoration: none; color: #ffffff; 
-	background-color: #5dbbd4; padding: 12px 10px 12px 10px; 
-	border-width: 0px;">
-	Post</button>
-
-<p style="color: #000;" id="paragraph"></p> */
