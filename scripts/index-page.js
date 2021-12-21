83a2ceb294d4cc3e1aae032c0ea4213f2d3fe281
Sayem Mohd Farhad <sayemfarhad001@gmail.com>
2021-12-21 16:19:38 -0500

    add deep dive delete post

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,128 +1,19 @@
-// All CODES added to Line 127 on index.html
-
-// Attempt 1 await/ async
-// let myKey = "";
-// copyKey().catch((error) => {
-// 	console.log(error);
-// });		// promise call - ends here
-
-// async function copyKey() {
-// 	const getKeyURL = await axios.get('https://project-1-api.herokuapp.com/register');
-// 	const getKey = getKeyURL.data.api_key 
-// 	//JSON.stringify(getKeyURL.data.api_key)
-// 	myKey = myKey + getKey;
-// }
-
-// console.log(myKey);
-
-
-// async function getComments() {
-// 	await copyKey();
-// 	let link = 'https://project-1-api.herokuapp.com/comments?api_key=' + myKey;
-// 	const getCommentsURL = axios.get(link);
-// 	const getComu = getCommentsURL.data;
-	
-// 	console.log(getComu)
-
-// }
-
-// getComments()
-
-// Attempt 2 - works on console but doesnt output data
-// let commentArray = []
-// let myKey = "";
-// async function copyKey() {
-// 	const getKeyURL = await axios.get('https://project-1-api.herokuapp.com/register');
-// 	const getKey = await getKeyURL.data.api_key;
-// 	myKey = myKey + getKey;
-// }
-// copyKey().catch((error) => {
-// 	console.log(error);
-// });		// promise call - ends here
-// console.log(myKey);
-
-// async function getComments() {
-// 	await copyKey();
-// 	let link = 'https://project-1-api.herokuapp.com/comments?api_key=' + myKey;
-// 	const getCommentsURL = await axios.get(link);
-// 	// const getComu = getCommentsURL.data;
-// 	console.log(getCommentsURL);
-// 	commentArray = getCommentsURL.data;
-
-// }
-// getComments().catch((error) => {
-// 	console.log(error);
-// });
-// console.log(commentArray);
-
-
-
-// let commentArray = [];
-
-// let getKeyURL = axios.get('https://project-1-api.herokuapp.com/register')
-// .then(result => {
-	
-// 	console.log(result.data.api_key);
-// 	// call whatEver() function created with target Array[result.data.data] as argument	
-// 	return result.data.api_key;
-	
-// 	// myKey = myKey + getKey;
-// 	// console.log(myKey);
-
-// 	// Add Text info after converting JSON to 'String'
-// 	// let selectAd = document.querySelector(".res__ad");
-// 	// let stringedText = JSON.stringify(result.data.support.text);
-// 	// selectAd.innerHTML = stringedText;
-// }) 
-// .catch((error) => {
-// console.log(error);
-// });		// promise call - ends here
-
+// Assigned API Key to global variable
 let myKey = 'b74217e9-d527-4bc2-90ae-26d03176c467';
 
+// Request the Comments Array from API
 let getCommentsURL = axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${myKey}`)
 .then(result => {
-	// return JSON.stringify(result.data);
 	return result.data
-	// console.log(JSON.stringify({ x: 5, y: 6 }));
-	// expected output: "{"x":5,"y":6}"
-}) 
+})
 .catch((error) => {
 console.log(error);
-});		// promise call - ends here
-
-
-
-// let postCommentsURL = axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${myKey}`)
-
-// 	Step 1 - create an editable Array with 3 default objects
-// let commentArray = [{
-// 	id: "0",
-// 	likes: 0,
-// 	name: "Connor Walton",
-// 	timestamp: "02/17/2021",
-// 	comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-// },
-// {
-// 	id: "1",
-// 	likes: 0,
-// 	name: "Emilie Beach",
-// 	timestamp: "01/09/2021",
-// 	comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-// },
-// {
-// 	id: "2",
-// 	likes: 0,
-// 	name: "Miles Acosta",
-// 	timestamp: "12/20/2020",
-// 	comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-// }];
+});
 
 // 	Step 2 - Create function to display Data from the Array above
 let displayComment = async () => {
 	let commentArray = await getCommentsURL;
-	for (i=0; i<commentArray.length; i++) {
-		
+	for (i=0; i<commentArray.length; i++) {		
 		// A - Create p element, add name from above Array and add class		
 		const nameTag = document.createElement('p');
 		nameTag.innerText = commentArray[i].name;
@@ -143,12 +34,35 @@ let displayComment = async () => {
 		const commentTextTag = document.createElement('p');
 		commentTextTag.innerText = commentArray[i].comment;
 		commentTextTag.classList.add("section__form-commentText");
+		// div - like and delete		
+		const likeButton = document.createElement('p');
+		likeButton.classList.add("like__button", "fa", "fa-thumbs-up");
+		likeButton.setAttribute("id", commentArray[i].id);
+
+		const likeCount = document.createElement('p');
+		likeCount.innerText = commentArray[i].likes;
+		likeCount.classList.add("like__count");
+
+		const likeDiv = document.createElement('div');
+		likeDiv.classList.add("section__form-likeDiv");
+		likeDiv.appendChild(likeButton);
+		likeDiv.appendChild(likeCount);
+
+		const deleteButton = document.createElement('p');
+		deleteButton.classList.add("delete__button", "fa", "fa-trash");
+		deleteButton.setAttribute("id", commentArray[i].id);
+		const innerDivBottom = document.createElement('div');
+		innerDivBottom.classList.add("section__form-innerDiv--bottom");
+
+		innerDivBottom.appendChild(likeDiv);
+		innerDivBottom.appendChild(deleteButton);
 		// F - Create OUTER div element, add class
 		const outerDiv = document.createElement('div');
 		outerDiv.classList.add("section__form-outerDiv");
 		// G - Append [D and E] to created OUTER div element(F)
 		outerDiv.appendChild(innerDiv);
 		outerDiv.appendChild(commentTextTag);
+		outerDiv.appendChild(innerDivBottom);
 		// H - Create p element[for logo], add class and NOTHING ELSE FOR NOW
 		const logoTag = document.createElement('p');
 		logoTag.classList.add("section__form-logo");
@@ -164,8 +78,6 @@ let displayComment = async () => {
 		commentSection.prepend(mainDiv);
 	}
 }
-
-// 	Step 3 - Call the created funtion - before any event
 displayComment()
 
 // 	Step 4 - Create an event for Clicking COMMENT Button
@@ -175,7 +87,6 @@ const sectionMain = document.querySelector(".section__form");
 //	B
 submitButton.addEventListener('click', async (event) => {
 	let commentArray = await getCommentsURL;
-
     // 	I. 	Prevent refreshing the page
 	event.preventDefault()
 	// 	II. Collect value from input element
@@ -196,18 +107,7 @@ submitButton.addEventListener('click', async (event) => {
 	// 	IV. Clear input fields
 	let clearAll = document.querySelector(".section__comment");
 	clearAll.innerHTML = "";
-	// 	V. Add Funtion for timestamp
-	// function getTimeStamp() {
-	// 	var now = new Date();
-	// 	return ( (now.getDate()) + '/' + (now.getMonth() + 1) + '/' + now.getFullYear());
- 	// }
-	//  VI. Create a new OBJECT to collect INPUT VALUE from FORM INPUT fields and a Time Funtion
-
-	//  VII. Add the created OBJECT[VI] to initial ARRAY[STEP 1]
-
-	// 	VIII. Call the created funtion[line 60] again - DUE TO CLICK EVENT
-
-    // 	IX. On Successful Submission, Remove Error Visual and Clear Input Fields
+    // 	V. On Successful Submission, Remove Error Visual and Clear Input Fields
 	const nameTag = document.querySelector("#name");
 	const commentTag = document.querySelector("#comment");
     // 	a. Remove Error Visual via removal and addition of classes
@@ -218,38 +118,107 @@ submitButton.addEventListener('click', async (event) => {
 	// 	b. Clear Input Fields
 	nameTag.value = "";
 	commentTag.value = "";
-
-
+	// 	VI.	Post request to the API 
 	axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${myKey}`, {name: nameValue, comment: commentValue})
 	.then(function (response) {
-		let newObject = { id: response.data.id, name: response.data.name, timestamp: response.data.timestamp, comment: response.data.comment }
-		// newObject.id = new Date().getTime();	
-		// newObject.name = nameValue;
-		// newObject.timestamp = new Date().getTime();
-		// newObject.comment = commentValue;
-		commentArray.push(newObject);
+		// let newObject = { id: response.data.id, likes: response.data.likes, name: response.data.name, timestamp: response.data.timestamp, comment: response.data.comment }
+		// commentArray.push(newObject);
+		location.reload()
 		displayComment()
 	})
 	.catch(function (error) { 
 		console.log(error);
 	});
-
-
 })
 
-// https://postman-echo.com/post
+// // clickLike on line 37
+
+// const addCount = document.querySelector(".like__count"); 
+
+// let like = 
+// increaseLike = (like) => {
+// 	like ++
+// 	addCount.innerHTML = `${like}`;
+// }
+// likeClick = (like) => {
+// 	increaseLike(like)
+// }
+
+// // // "id", commentArray[i].id
+
+// let clickLike = document.querySelector(".like__button");
+// clickLike.addEventListener('click', async (event) => {
+// 	await getCommentsURL;
+// 	event.stopPropagation();
+// 	const count = document.querySelector(".like__count");
+// 	let currentLikes = count.innerText;
+
+// 	let id = clickLike.getAttribute("id");
+// 	let putURL = axios.put(`https://project-1-api.herokuapp.com/comments/${id}/like?api_key=${myKey}`, {"likes" : currentLikes++ })
+// 	putURL.then(function () {	
+// 		// likeClick(response.data.likes)
+// 		displayComment()
+// 	})
+// 	.catch(function (error) { 
+// 		console.log(error);
+// 	})
+// })
 
 
 
 
 
-// let postCommentsURL = axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${myKey}`)
-// .then(result => {
-// 	// return JSON.stringify(result.data);
-// 	return result.data
-// 	// console.log(JSON.stringify({ x: 5, y: 6 }));
-// 	// expected output: "{"x":5,"y":6}"
-// }) 
-// .catch((error) => {
-// console.log(error);
-// });		// promise call - ends here
+
+
+
+
+
+
+// clickLike on line 41
+// Delete post using async/await
+
+// const clickDelete = document.querySelectorAll(".delete__button");
+// const selectComment = document.querySelectorAll(".section__form-mainDiv");
+// const getCommentId = selectComment.getAttribute(".section__form-mainDiv");
+
+// "id", commentArray[i].id
+async function addDeleteEvent() {
+	let commentArray = await getCommentsURL;
+	const clickDelete = document.querySelectorAll(".delete__button")
+	if (commentArray){
+		for (var i = 0; i < clickDelete.length; i++) {
+			let id = clickDelete[i].getAttribute("id");
+			clickDelete[i].addEventListener('click', (event) => {
+				if (window.confirm("Are you sure you want to delete this post?")) {
+					event.preventDefault();
+					axios.delete(`https://project-1-api.herokuapp.com/comments/${id}?api_key=${myKey}`)
+					.then((response) => {
+						location.reload()
+						// displayComment()						
+					}) 
+					.catch((error) => { 
+						console.log(error)
+					})
+				}
+
+				// displayComment()
+			}); 
+		}
+
+	}
+}
+
+addDeleteEvent()
+// .then (()=>{
+
+// 	addDeleteEvent();
+// })
+			// window.confirm("Are you sure you want to delete this post?")
+		// function deleteComment() {
+		// 	let res = axios.delete(`https://project-1-api.herokuapp.com/comments/${id}?api_key=${myKey}`);
+		// 	if (commentArray.data.id === getCommentId) {
+		// 		commentArray[i].remove();
+		// 	}	
+		// 	console.log(res.status);
+		// }
+		// deleteComment();
